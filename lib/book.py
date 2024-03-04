6519c97461f64c2a00cc1f180222523bb291c3c8
SengokuGarp <garpirungu@gmail.com>
2024-03-04 10:03:29 +0300

    Initial commit

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,22 @@
-#!/usr/bin/env python3
+# lib/book.py
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title, pages, price):
+        self.title = title
+        self.pages = pages
+        self.price = price
+
+    def get_title(self):
+        return self.title
+
+    def get_pages(self):
+        return self.pages
+
+    def get_price(self):
+        return self.price
+
+    def turn_page(self):
+        return "Flipping the page...wow, you read fast!"
+
+    def __str__(self):
+        return f"{self.title}"
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,28 @@
-#!/usr/bin/env python3
+# lib/shoe.py
 
 class Shoe:
-    pass
+    def __init__(self, brand, size, color, price):
+        self.brand = brand
+        self.size = size
+        self.color = color
+        self.price = price
+        self.condition = "New"
+
+    def get_brand(self):
+        return self.brand
+
+    def get_size(self):
+        return self.size
+
+    def get_color(self):
+        return self.color
+
+    def get_price(self):
+        return self.price
+
+    def cobble(self):
+        self.condition = "New"
+        return "The shoe has been repaired."
+
+    def __str__(self):
+        return f"{self.brand} {self.color}, Size {self.size}"
